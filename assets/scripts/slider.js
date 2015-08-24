801adf9128a35f2de977a3b5a76d7fc5312d706a
John Daskovsky <johndaskovsky@gmail.com>
2015-08-24 16:56:34 -0400

    slider

---
+++ assets/scripts/slider.js
@@ -0,0 +1,10 @@
+var slider = new MasterSlider();
+slider.setup('masterslider' , {
+        width:1400,    // slider standard width
+        height:500,   // slider standard height
+        space:0
+        // more slider options goes here...
+        // check slider options section in documentation for more options.
+    });
+// adds Arrows navigation control to the slider.
+slider.control('arrows');

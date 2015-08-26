dacf2b9f569195d59f64f31468aa808a3fe2eb44
John Daskovsky <johndaskovsky@gmail.com>
2015-08-25 21:25:16 -0400

    slider style

--- assets/scripts/build/production.js
+++ assets/scripts/build/production.js
@@ -14225,6 +14225,21 @@ $(function () {
 });
 $(document).ready(function(){
   $('.full-width-slider').slick({
+    lazyLoad: 'ondemand',
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 500
+  });
+});
+
+$(document).ready(function(){
+  $('.multi-slide-sm').slick({
+    lazyLoad: 'ondemand'
+  });
+});
+
+$(document).ready(function(){
+  $('.multi-slide-lg').slick({
     lazyLoad: 'ondemand'
   });
 });
--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
   $('.full-width-slider').slick({
+    lazyLoad: 'ondemand',
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 500
+  });
+});
+
+$(document).ready(function(){
+  $('.multi-slide-sm').slick({
+    lazyLoad: 'ondemand'
+  });
+});
+
+$(document).ready(function(){
+  $('.multi-slide-lg').slick({
     lazyLoad: 'ondemand'
   });
 });

bdad1fb6dcdb71fcd4470e251428118215427ec0
John Daskovsky <johndaskovsky@gmail.com>
2015-08-27 09:58:32 -0400

    user icon and toggle mobile nav icon

--- assets/scripts/build/production.js
+++ assets/scripts/build/production.js
@@ -14200,13 +14200,13 @@ return jQuery;
 }));
 
 // Affix nav
-
 $('nav').affix({
   offset: {
     top: $('header').height()
   }
 });
 
+//Slide down dropdowns
 $(document).ready(function(){
   $('.dropdown').on('show.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
@@ -14214,6 +14214,19 @@ $(document).ready(function(){
   $('.dropdown').on('hide.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
   });
+});
+
+//Toggle mobile toggle menu icon with cross
+$(function() {
+  $('#navbar')
+    .on('shown.bs.collapse', function() {
+      $('#nav-toggle-icon').addClass('hidden');
+      $('#nav-toggle-cross').removeClass('hidden');    
+    })
+    .on('hidden.bs.collapse', function() {
+      $('#nav-toggle-icon').removeClass('hidden');
+      $('#nav-toggle-cross').addClass('hidden');        
+    });
 });
 
 $(function () {
--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -1,11 +1,11 @@
 // Affix nav
-
 $('nav').affix({
   offset: {
     top: $('header').height()
   }
 });
 
+//Slide down dropdowns
 $(document).ready(function(){
   $('.dropdown').on('show.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
@@ -13,4 +13,17 @@ $(document).ready(function(){
   $('.dropdown').on('hide.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
   });
+});
+
+//Toggle mobile toggle menu icon with cross
+$(function() {
+  $('#navbar')
+    .on('shown.bs.collapse', function() {
+      $('#nav-toggle-icon').addClass('hidden');
+      $('#nav-toggle-cross').removeClass('hidden');    
+    })
+    .on('hidden.bs.collapse', function() {
+      $('#nav-toggle-icon').removeClass('hidden');
+      $('#nav-toggle-cross').addClass('hidden');        
+    });
 });
